4dcfde37bcc4d33a59fc17155f9065409cddffa1
James Saryerwinnie <js@jamesls.com>
2025-05-26 20:26:26 -0400

    Add additional languages
    Also standardize on just the plain old language
    name instead of language codes.

--- src/jaudible/voices.py
+++ src/jaudible/voices.py
@@ -1,9 +1,27 @@
 LANGUAGES = {
-    'en': {
+    'english': {
         'voice': 'Matthew',
         'engine': 'generative',
         'language_code': 'en-US',
     },
-    'fr': {'engine': 'generative', 'voice': 'Lea', 'language_code': 'fr-FR'},
-    'es': {'engine': 'generative', 'voice': 'Mia', 'language_code': 'es-MX'},
+    'french': {
+        'engine': 'generative',
+        'voice': 'Lea',
+        'language_code': 'fr-FR',
+    },
+    'spanish': {
+        'engine': 'generative',
+        'voice': 'Mia',
+        'language_code': 'es-MX',
+    },
+    'cantonese': {
+        'engine': 'neural',
+        'voice': 'Hiujin',
+        'language_code': 'yue-CN',
+    },
+    'mandarin': {
+        'engine': 'neural',
+        'voice': 'Zhiyu',
+        'language_code': 'cmn-CN',
+    },
 }
